d5f4031aa5027e8f7b0ff3cd153eb429322fd56b
J-Mario-García <jm.gcaxaj@udeo.edu.gt>
2025-08-07 22:35:34 -0600

    Agregar match y programa principal

--- Actividad_13.py
+++ Actividad_13.py
@@ -2,9 +2,9 @@ def ordenar(lista):
     if len(lista) <= 1:
         return lista
     pivote = lista[0]
-    mayores = [x for x in lista[1:] if x.paquetes > pivote]
-    iguales = [x for x in lista if x.paquetes == pivote]
-    menores = [x for x in lista[1:] if x.paquetes < pivote]
+    mayores = [x for x in lista[1:] if x.paquetes > pivote.paquetes]
+    iguales = [x for x in lista if x.paquetes == pivote.paquetes]
+    menores = [x for x in lista[1:] if x.paquetes < pivote.paquetes]
     return ordenar(mayores) + iguales + ordenar(menores)
 
 class Repartidor:
@@ -40,21 +40,28 @@ class Repartidor:
         else:
             raise ValueError("Zona ingregada no válida")
     def __str__(self):
-        return f"Nombre: {self.nombre}, Paquetes: {self.paquetes}, zona: {self.zona}"
+        return f"{self.nombre} - {self.paquetes} paquetes - Zona: {self.zona}"
 
 class EmpresaMensajeria:
-    repartidores = []
+    def __init__(self):
+        self.repartidores = []
     def agregar_repartidor(self, repartidor):
         if repartidor.nombre in self.repartidores:
             print("Ya se ha registrado un repartidor con el nombre ingresado")
         else:
             self.repartidores.append(repartidor)
             print("El registro se completo con éxito")
-    def mostrar_ranking(self):
-        ranking = ordenar(self.repartidores)
-        print("Ranking de repartidores:")
-        for x in ranking:
-            print(x)
+    def mostrar(self):
+        if not self.repartidores:
+            print("No se ha registrado ningún repartidor")
+        else:
+            print("Repartidores registrados:")
+            for x in self.repartidores:
+                print(x)
+            ranking = ordenar(self.repartidores)
+            print("Ranking de repartidores:")
+            for x in ranking:
+                print(x)
     def buscar_repartidor(self, nombre):
         for repartidor in self.repartidores:
             if repartidor.nombre.lower() == nombre.lower():
@@ -70,5 +77,43 @@ class EmpresaMensajeria:
         print("\nESTADÍSTICAS:")
         print(f"Total de paquetes entregados: {total_paquetes}")
         print(f"Promedio de paquetes entregados: {promedio_paquetes:.2f}")
-        print(f"Repartidor(es) con más entregas: ):{', '.join(mayor_repartidor)}")
-        print(f"Repartidor(es) con menos entregar: ):{', '.join(menor_repartidor)}")
+        print(f"Repartidor(es) con más entregas: {', '.join(mayor_repartidor)}")
+        print(f"Repartidor(es) con menos entregar: {', '.join(menor_repartidor)}")
+
+empresa = EmpresaMensajeria()
+opcion = "0"
+while opcion != "5":
+    print("==SISTEMA MENSAJERÍA==")
+    print("1.Registrar repartidor")
+    print("2.Mostrar repartidores registrados")
+    print("3.Buscar repartidor")
+    print("4.Mostrar estadísticas")
+    print("5.Salir")
+    opcion = input("\nSeleccione una opción: ")
+    match opcion:
+        case "1":
+            try:
+                cantidad = int(input("¿Cuántos repartidores desea registrar?: "))
+                for i in range(cantidad):
+                    print(f"Ingrese los datos del repartidor {i + 1}:")
+                    nombre = input("\tNombre: ")
+                    paquetes = int(input("\tPaquetes: "))
+                    zona = input("\tZona: ")
+                    repartidor = Repartidor(nombre, paquetes, zona)
+                    empresa.agregar_repartidor(repartidor)
+            except ValueError as e:
+                print(e)
+        case "2":
+            empresa.mostrar()
+        case "3":
+            if not empresa.repartidores:
+                print("No se ha registrado ninguna repartidor")
+                continue
+            buscar_nombre = input("Ingrese el nombre que desea consultar: ")
+            resultado = empresa.buscar_repartidor(buscar_nombre)
+            print(resultado)
+        case "4":
+            if not empresa.repartidores:
+                print("Aún no hay estadisticas, registre al menos un repatidor")
+                continue
+            empresa.estadisticas()
